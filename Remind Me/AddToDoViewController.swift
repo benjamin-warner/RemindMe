e68c906b82dc79c047eaac07e2ab5d66e67cf70d
Ben Warner <benjaminnwarner@gmail.com>
2018-05-05 10:54:37 -0400

    Layout added

---
+++ Remind Me/AddToDoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddToDoViewController.swift
+//  Remind Me
+//
+//  Created by Benjamin Warner on 5/5/18.
+//  Copyright © 2018 Benjamin Warner. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    @IBOutlet weak var todoText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onSubmit(_ sender: UIButton) {
+    }
+    
+}
